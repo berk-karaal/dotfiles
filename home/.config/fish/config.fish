48cf0c1f9e83b0faa33f860c62e0e2aa8cf21c8b
berk-karaal <karaalberk7@gmail.com>
2026-01-25 23:06:19 +0300

    update fish config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,9 +2,15 @@ function starship_transient_prompt_func
     starship module character
 end
 
+bind alt-backspace backward-kill-word
+
+test -f /home/berk/kubectl-aliases/.kubectl_aliases.fish && source /home/berk/kubectl-aliases/.kubectl_aliases.fish
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+    set PATH "/home/berk/bin/" $PATH
+    set PATH "~/bin" $PATH
     set PATH "/home/berk/.local/bin/" $PATH
     set PATH "/home/berk/.cargo/bin/" $PATH
     set PATH "/home/berk/.local/share/JetBrains/Toolbox/scripts" $PATH
@@ -14,3 +20,10 @@ if status is-interactive
     starship init fish | source
     enable_transience
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+set --export ENABLE_LSP_TOOL 1
+
