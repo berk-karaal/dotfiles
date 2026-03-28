3b5beb1146ba2010a12b29ebb63f6d31053fe435
Berk Karaal <karaalberk7@gmail.com>
2026-03-29 00:23:08 +0300

    Refactor paths to use home directory shortcut
    Updated file paths in config.fish to use '~' for home directory.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,18 +4,18 @@ end
 
 bind alt-backspace backward-kill-word
 
-test -f /home/berk/kubectl-aliases/.kubectl_aliases.fish && source /home/berk/kubectl-aliases/.kubectl_aliases.fish
+test -f ~/kubectl-aliases/.kubectl_aliases.fish && source ~/kubectl-aliases/.kubectl_aliases.fish
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
-    set PATH "/home/berk/bin/" $PATH
+    set PATH "~/bin/" $PATH
     set PATH "~/bin" $PATH
-    set PATH "/home/berk/.local/bin/" $PATH
-    set PATH "/home/berk/.cargo/bin/" $PATH
-    set PATH "/home/berk/.local/share/JetBrains/Toolbox/scripts" $PATH
+    set PATH "~/.local/bin/" $PATH
+    set PATH "~/.cargo/bin/" $PATH
+    set PATH "~/.local/share/JetBrains/Toolbox/scripts" $PATH
     set PATH "/usr/local/go/bin" $PATH
-	set PATH "/home/berk/go/bin" $PATH
+	set PATH "~/go/bin" $PATH
     set XDG_CONFIG_HOME $HOME/.config
     starship init fish | source
     enable_transience
